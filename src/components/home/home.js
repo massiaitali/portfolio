536856b6d9197957af2a9ac60443dded07a5c7fc
massinissa <massinissa.aitali@axa.fr>
2019-07-22 17:36:13 +0200

    feat(component): create home

---
+++ src/components/home/home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+	return (
+		<div className="section" id="home">
+                <h5 className="center text-grey margin-top-15">Looking for a position in</h5>
+                <h1 className="center margin-top-5">
+                    Software Engineer
+                </h1>
+                <h6 className="center text-grey margin-top-15">In Los Angeles</h6>
+        </div>
+  );
+}
+
+export default Home;
+
