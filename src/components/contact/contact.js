5d1d07c70d582c353882e9ab0e9077b685f0f2cb
massinissa <massinissa.aitali@axa.fr>
2019-07-22 17:35:34 +0200

    feat(component): create contact

---
+++ src/components/contact/contact.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './contact.css';
+
+const Contact = () => {
+  return (
+
+    <div id={'contact'} className={'section'}>
+      <div className="row center">
+        <div className={'col s12 m4'} >
+          <a className="link" href="mailto:massi.aitali@gmail.com">
+            <i
+              className="fa fa-envelope-o contact-size"></i>
+          </a>
+        </div>
+        <div className={'col s12 m4'} >
+        <a className="link"
+           href="https://www.linkedin.com/in/massinissa-ait-ali-07711aba/">
+          <i
+            className="fa fa-linkedin contact-size"></i>
+        </a>
+        </div>
+        <div className={'col s12 m4'} >
+        <a className="link" href="https://github.com/massiaitali">
+          <i
+            className="fa fa-github contact-size"></i>
+        </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
+
