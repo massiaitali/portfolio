61d6563996489f66e29f8c5a57b807b234931a11
massi <massi.aitali@gmail.com>
2019-08-07 23:03:57 +0200

    feat: frnech in France and english for other countries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from './components/navbar/navbar';
 import Home from './components/home/home';
 import Educations from './components/educations/educations';
 import Works from './components/works/works';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
+import axios from 'axios'
 
 import "materialize-css/dist/css/materialize.min.css";
 import './App.css';
 
 import data from './dataPortfolio'
 
+const getGeoInfo = (changeLang) => {
+    axios.get('https://ipapi.co/json/').then((response) => {
+        const dataJson = response.data;
+        dataJson.country === 'FR' && changeLang('fr');
+    }).catch((error) => {
+        return false;
+    });
+};
+
 const App = () => {
 	const [lang, useLang] = useState('en');
+
+	useEffect(() => {
+	    getGeoInfo(useLang);
+	}, []);
 
 	const dataPortfolio = data[lang];
 
